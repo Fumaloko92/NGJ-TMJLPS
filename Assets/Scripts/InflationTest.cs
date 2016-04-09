1199112cd66e4a7be0adcf1f35ddf8fd79e2ed12
Jacob Korsgaard <felizk@felizk.dk>
2016-04-09 23:19:51 +0200

    Luca do sounds

---
+++ Assets/Scripts/InflationTest.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class InflationTest : MonoBehaviour {
+
+    public Animator animator;
+    public SkinnedMeshRenderer skinnedMesh;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        animator.SetFloat("MorphArgument", (Time.time % 1f) * 1);
+        skinnedMesh.SetBlendShapeWeight(0, (Time.time % 1f) * 100f);
+	}
+}
