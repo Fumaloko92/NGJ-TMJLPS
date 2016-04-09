c17167c78f8cf019dfa6ef2087dd9b9ed8d02562
Luca Salfi <lucasalfi2@gmail.com>
2016-04-09 10:47:29 +0200

    Changes to be committed: 	new file:   NGJ-UnityProjectFolder/Assets/Models.meta 	new file:   NGJ-UnityProjectFolder/Assets/Models/Sphere.prefab 	new file:   NGJ-UnityProjectFolder/Assets/Models/Sphere.prefab.meta 	new file:   NGJ-UnityProjectFolder/Assets/PhysicsMaterial.meta 	new file:   NGJ-UnityProjectFolder/Assets/PhysicsMaterial/Rubber.physicMaterial 	new file:   NGJ-UnityProjectFolder/Assets/PhysicsMaterial/Rubber.physicMaterial.meta 	modified:   NGJ-UnityProjectFolder/Assets/Scenes/test.unity 	modified:   NGJ-UnityProjectFolder/Assets/Scripts/BaloonSimple.cs 	modified:   NGJ-UnityProjectFolder/Library/CurrentLayout.dwlt 	modified:   NGJ-UnityProjectFolder/Library/InspectorExpandedItems.asset 	modified:   NGJ-UnityProjectFolder/Library/ScriptAssemblies/Assembly-CSharp.dll 	modified:   NGJ-UnityProjectFolder/Library/ScriptAssemblies/Assembly-CSharp.dll.mdb 	new file:   NGJ-UnityProjectFolder/Library/UnityAssemblies/UnityEditor.dll 	new file:   NGJ-UnityProjectFolder/Library/UnityAssemblies/UnityEditor.xml 	new file:   NGJ-UnityProjectFolder/Library/UnityAssemblies/UnityEngine.Networking.dll 	new file:   NGJ-UnityProjectFolder/Library/UnityAssemblies/UnityEngine.Networking.xml 	new file:   NGJ-UnityProjectFolder/Library/UnityAssemblies/UnityEngine.UI.dll 	new file:   NGJ-UnityProjectFolder/Library/UnityAssemblies/UnityEngine.UI.xml 	new file:   NGJ-UnityProjectFolder/Library/UnityAssemblies/UnityEngine.dll 	new file:   NGJ-UnityProjectFolder/Library/UnityAssemblies/UnityEngine.xml 	new file:   NGJ-UnityProjectFolder/Library/UnityAssemblies/version.txt 	modified:   NGJ-UnityProjectFolder/Library/assetDatabase3 	modified:   NGJ-UnityProjectFolder/Library/expandedItems 	modified:   NGJ-UnityProjectFolder/Library/metadata/00/00000000000000002000000000000000 	modified:   NGJ-UnityProjectFolder/Library/metadata/00/00000000000000004000000000000000 	modified:   NGJ-UnityProjectFolder/Library/metadata/00/00000000000000006100000000000000 	new file:   NGJ-UnityProjectFolder/Library/metadata/1d/1dfd4a8f4b1af3f468b7cefe8e0f5389 	new file:   NGJ-UnityProjectFolder/Library/metadata/1d/1dfd4a8f4b1af3f468b7cefe8e0f5389.info 	new file:   NGJ-UnityProjectFolder/Library/metadata/37/374063b3bdde07f478083f9bae5e1166 	new file:   NGJ-UnityProjectFolder/Library/metadata/37/374063b3bdde07f478083f9bae5e1166.info 	new file:   NGJ-UnityProjectFolder/Library/metadata/42/429b3e9ba300f7f4ca4e172b889f8718 	new file:   NGJ-UnityProjectFolder/Library/metadata/42/429b3e9ba300f7f4ca4e172b889f8718.info 	new file:   NGJ-UnityProjectFolder/Library/metadata/5e/5e63ebadd5149924ca1373d56e376060 	new file:   NGJ-UnityProjectFolder/Library/metadata/5e/5e63ebadd5149924ca1373d56e376060.info 	modified:   NGJ-UnityProjectFolder/Library/metadata/b2/b2bc2fcc2db420d45ab4911acb40fc9e 	modified:   NGJ-UnityProjectFolder/Library/shadercompiler-UnityShaderCompiler.exe0.log 	modified:   NGJ-UnityProjectFolder/ProjectSettings/InputManager.asset

--- NGJ-UnityProjectFolder/Assets/Scripts/BaloonSimple.cs
+++ NGJ-UnityProjectFolder/Assets/Scripts/BaloonSimple.cs
@@ -1,15 +1,24 @@
 ﻿using UnityEngine;
 using System.Collections;
-
+[RequireComponent(typeof(Rigidbody))]
 public class BaloonSimple : MonoBehaviour {
-
+    [SerializeField]
+    private string horizontalAxisName;
+    [SerializeField]
+    private string verticalAxisName;
+    [SerializeField]
+    private float force;
+    private Rigidbody _body;
 	// Use this for initialization
-	void Start () {
-	
+	void Awake () {
+        _body = GetComponent<Rigidbody>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+        float h = Input.GetAxis(horizontalAxisName);
+        float v = Input.GetAxis(verticalAxisName);
+        Vector3 position = new Vector3(h, v);
+        _body.AddForce(position , ForceMode.Impulse);
 	}
 }
