ef564a039d47c885d8ee3b3ed8911b165b32132f
Luca Salfi <lucasalfi2@gmail.com>
2016-04-10 09:03:00 +0200

    Text and effects

---
+++ Assets/startingEffectManager.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class startingEffectManager : MonoBehaviour {
+    [SerializeField]
+    private GameObject[] textForEverySecond;
+
+    private float _timer;
+    private int _index;
+    void Awake()
+    {
+        _index = 0;
+        _timer = 0;
+    }
+    void Update()
+    {
+        _timer += Time.deltaTime;
+        if(_timer>1&&_index<textForEverySecond.Length)
+        {
+            if (_index > 0)
+                textForEverySecond[_index - 1].SetActive(false);
+            textForEverySecond[_index].SetActive(true);
+            _index++;
+            _timer = 0;
+        }
+    }
+}
