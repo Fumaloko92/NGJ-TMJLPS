864ac426a51c7e787a4a004453e40b2adf072f4e
Luca Salfi <lucasalfi2@gmail.com>
2016-04-10 09:33:49 +0200

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	Assets/Main.unity

--- Assets/Scripts/startingEffectManager.cs
+++ Assets/Scripts/startingEffectManager.cs
@@ -4,16 +4,21 @@ using System.Collections;
 public class startingEffectManager : MonoBehaviour {
     [SerializeField]
     private GameObject[] textForEverySecond;
-
+    
+    private GameObject[] _players;
     private float _timer;
     private int _index;
     void Awake()
     {
         _index = 0;
         _timer = 0;
+        _players = GameObject.FindGameObjectsWithTag("Player");
+        foreach(GameObject p in _players)
+            GetComponentInChildren<InflateFish>().setInflation(false);
     }
     void Update()
     {
+        if(_timer<=1)
         _timer += Time.deltaTime;
         if(_timer>1&&_index<textForEverySecond.Length)
         {
@@ -22,6 +27,11 @@ public class startingEffectManager : MonoBehaviour {
             textForEverySecond[_index].SetActive(true);
             _index++;
             _timer = 0;
+           
         }
+        if(_index>=textForEverySecond.Length)
+            foreach (GameObject p in _players)
+                GetComponentInChildren<InflateFish>().setInflation(true);
     }
+    
 }
