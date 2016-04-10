52d499d704948f5b5d4f16a4ffbf2659393da5ab
Jacob Korsgaard <felizk@felizk.dk>
2016-04-10 10:00:18 +0200

    Added fish collision

---
+++ Assets/Scripts/FishCollision.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FishCollision : MonoBehaviour
+{
+    private float oldSpeed;
+    private float speed;
+    private Rigidbody rb;
+    private PlayerHealth ph;
+ 
+    void Start()
+    {
+        ph = GetComponent<PlayerHealth>();
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void FixedUpdate()
+    {
+        oldSpeed = speed;
+        speed = rb.velocity.magnitude;
+    }
+
+    void OnCollisionEnter(Collision col)
+    {
+        var otherCollision = col.gameObject.GetComponent<FishCollision>();
+        if (col.gameObject.tag == "Player")
+        {
+            var otherRigidBody = col.rigidbody;
+
+            if(speed > 5)
+            {
+                col.gameObject.GetComponent<PlayerHealth>().dealDamage(20);
+            }
+        }
+    }
+}
